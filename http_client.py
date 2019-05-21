1b5235f21e28789f94a2bbf8744070f9779d0e25
yooner <411890573@qq.com>
2019-05-21 22:39:40 +0800

    test

--- http_client.py
+++ http_client.py
@@ -1,8 +1,9 @@
 import socket
+import sys
 
 
 address = ('127.0.0.1', 80)
-def client():
+def client(*argv):
     '''
     GET / HTTP/1.1\r\n
     Host: baidu.com\r\n
@@ -12,11 +13,19 @@ def client():
     Accept-Encoding:gzip, deflate\r\n
     Accept-Language:zh-CN,zh;q=0.9,en;q=0.8,en-US;q=0.7\r\n
     Cookie:xx=xx;yy=yy\r\n
+    ----
+    useage: http_client.py ip:port/path
     '''
     
+    if len(argv):
+        pass
     with socket.socket(family=AF_INET, type=SOCKET_STEAM, proto=0, fileno=None) as s:
         s.connect(address)
         s.sendall(b'hello')
         data = s.recv(1024)
     print(data)
     
+
+if __name__ == '__main__':
+    if len(sys.argv) >= 2:
+        pass
