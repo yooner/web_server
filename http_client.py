e14e5d273377c625f07e4493aa0fef0e68690069
twonaviersplayweb <playweb@163.com>
2019-05-22 08:48:26 +0800

    add check ip port method

--- http_client.py
+++ http_client.py
@@ -25,7 +25,60 @@ def client(*argv):
         data = s.recv(1024)
     print(data)
     
+def useage(print_str=None):
+    if print_str:
+        print(print_str)
+    print("useage: http_client.py ip:port/path get/post [postting]")
+    
+def check_ip(ip):
+    split_ip_list = ip.split('.')
+    if len(split_ip_list) != 4:
+        useage('ip format error')
+    try:
+        split_ip_list = [0<= int(num) <=255 for num in split_ip_list]
+    except ValueError as e:
+        useage('ip format error')
+        return False
+    
+    if set(split_ip_list) != {True}:
+        useage('ip format error')
+        return False
+    return True        
+
+def check_port(port):
+    if not port:
+        useage('port format error')
+        return False
+    try:
+        port = int(port)
+    except ValueError as e:
+        useage('port format error')
+        return False
+    return True
+
+def check_method(method):
+    method_list = ['GET', 'POST']
+    if method.upper() not in method_list:
+        usegae('method format error')
+
 
 if __name__ == '__main__':
-    if len(sys.argv) >= 2:
-        pass
+    if len(sys.argv) < 5:
+        useage()
+    
+    # parse ip and url
+    ip_and_url = argv[1]
+
+    ip = argv[1].partition(':')[0]
+    if !check_ip(ip):
+        return 
+    port = argv[1].partition('/')[0].partition(':')[2]
+    if !check_port(port):
+        return
+    url = '/' + argv[1].partition('/')[2]
+
+    # parse method
+    method = argv[2]
+
+
+
